038e3740e858ec9d1c8ec2ff706f12ccc82b0072
gawasa29 <yusei112929@gmail.com>
2025-06-13 15:24:14 +0900

    add sign out

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
+import { SignOut } from "@/components/SignOut"
 import { SignIn } from "@/components/SingIn"
 import { Button, HStack } from "@chakra-ui/react"
+import { auth } from "../../auth"
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth()
+
   return (
     <HStack>
       <Button>Click me</Button>
       <SignIn />
+      <SignOut />
+      {session?.user?.name}
+      {session?.user?.email}
+      {session?.user?.id}
+      {session?.user?.image}
     </HStack>
   )
 }
